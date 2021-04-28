452a2ffa0889032a835acfb529b95f527094dfdc
My Name <belkatan@yandex.ru>
2021-04-28 03:18:23 +0300

    pure main - work

--- src/test/java/pure/autotests/helpers/DriverHelper.java
+++ src/test/java/pure/autotests/helpers/DriverHelper.java
@@ -1,71 +1,73 @@
 package pure.autotests.helpers;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import pure.autotests.config.DriverConfig;
+import com.codeborne.selenide.Configuration;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import pure.autotests.tests.TestData;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.codeborne.selenide.Selenide.getWebDriverLogs;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static java.lang.String.join;
 import static org.openqa.selenium.logging.LogType.BROWSER;
 
 public class DriverHelper {
 
-    private static DriverConfig getDriverConfig() {
+    private static DriverConfig getDriverConfig(){
         return ConfigFactory.newInstance().create(DriverConfig.class, System.getProperties());
     }
 
-    public static String getWebMobile() {
+    public static String getWebMobile(){
         return getDriverConfig().webBrowserMobileView();
     }
 
-    public static boolean isWebMobile() {
+    public static boolean isWebMobile(){
         return !getWebMobile().equals("");
     }
 
-    public static String getWebRemoteDriver() {
+
+    public static String getWebRemoteDriver(){
         // https://%s:%s@selenoid.autotests.cloud/wd/hub/
         return String.format(getDriverConfig().webRemoteDriverUrl(),
                 getDriverConfig().webRemoteDriverUser(),
                 getDriverConfig().webRemoteDriverPassword());
     }
 
-    public static boolean isRemoteWebDriver() {
+    public static boolean isRemoteWebDriver(){
         return !getDriverConfig().webRemoteDriverUrl().equals("");
     }
 
-    public static String getVideoUrl() {
+    public static String getVideoUrl(){
         return getDriverConfig().videoStorage();
     }
 
-    public static boolean isVideoOn() {
+    public static boolean isVideoOn(){
         return !getVideoUrl().equals("");
     }
 
     public static String getSessionId(){
-        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString().replace("selenoid","");
+        return ( (RemoteWebDriver) getWebDriver() ).getSessionId().toString().replace("selenoid", "");
     }
 
-    public static String getConsoleLogs() {
-        return String.join("\n", Selenide.getWebDriverLogs(BROWSER));
+    public static String getConsoleLogs(){
+        return join("\n", getWebDriverLogs(BROWSER));
     }
 
-    public static void configureDriver() {
+    public static void configureDriver(){
+        Configuration.startMaximized = true;
         addListener("AllureSelenide", new AllureSelenide());
+
+//        Configuration.baseUrl = TestData.getWebUrl();
 
         Configuration.browser = getDriverConfig().webBrowser();
         Configuration.browserVersion = getDriverConfig().webBrowserVersion();
         Configuration.browserSize = getDriverConfig().webBrowserSize();
-        Configuration.baseUrl = TestData.getWebUrl();
-        Configuration.timeout = 10000;
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
@@ -84,6 +86,5 @@ public class DriverHelper {
         }
 
         Configuration.browserCapabilities = capabilities;
-
     }
 }
